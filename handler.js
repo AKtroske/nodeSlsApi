a47e68132e7c1d730b06dda3c2dae7dcbc27551e
Akahi Troske <akahitroske@gmail.com>
2020-12-30 22:59:52 -0800

    Add user base

--- handler.js
+++ handler.js
@@ -6,21 +6,4 @@ const server = serverlessExpress.createServer(app);
 module.handler = (event, context) => {
   console.log('event handled')
   return serverlessExpress.proxy(server, event, context);
-}
-
-// module.exports.hello = async event => {
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify(
-//       {
-//         message: 'Go Serverless v1.0! Your function executed successfully!',
-//         input: event,
-//       },
-//       null,
-//       2
-//     ),
-//   };
-//
-//   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-//   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-// };
+};
---
+++ src/user/routes.js
@@ -0,0 +1,5 @@
+const express = require('express');
+
+const routes = express.router(
+  mergeParams = true
+);
